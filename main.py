b35cdc9b5584af189ad934cc33a8b430622fef02
gravizapin <gravizapin@mail.ru>
2021-08-22 09:15:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+a =100
+b =300
+
+result = a + b
+res_a = a * b
+res_b = b - a
+res_c = b / a
+res_d = b // a
+res_e = b % a
+print(result)
+print(res_a)
+print(res_b)
+print(res_c)
+print(res_d)
+print(res_e)
